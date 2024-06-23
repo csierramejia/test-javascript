a682292ae29c42c696fecd8254d03a09b5720170
Cesar Sierra <cesar.sierra.mejia@gmail.com>
2024-06-22 21:02:28 -0500

    blackjack / cards

--- fundamental/js/app.js
+++ fundamental/js/app.js
@@ -1,23 +1,27 @@
+(() => {
+    'use strict'
 
-const people = [
-    {
-        name: 'Juan',
-        age: 38
-    },
-    {
-        name: 'Maria',
-        age: 36
-    }
-];
+    const people = [
+        {
+            name: 'Juan',
+            age: 38
+        },
+        {
+            name: 'Gina',
+            age: 36
+        }
+    ];
 
-console.table(people);
+    console.table(people);
 
-console.info('Test Info');
-console.warn('Test warn');
-console.error('Test error');
+    console.info('Test Info');
+    console.warn('Test warn');
+    console.error('Test error');
 
-let a = 10, b = 20, c = 30;
+    let a = 10, b = 20, c = 30;
 
-console.log ( {a} );
+    console.log({ a });
 
-console.table ( { a, b, c} );
+    console.table({ a, b, c });
+
+})();
